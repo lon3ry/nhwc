1062dd995b308949c1360d66db82b17484e538fe
Maxim Gurinov <lon3ry@ya.ru>
2026-09-07 23:23:51 +0300

    Add LRU cache example

---
+++ src/lru.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <cstddef>
+#include <list>
+#include <unordered_map>
+
+namespace caches {
+
+template <typename T, typename KeyT = int> class LRUCache {
+    std::size_t size_;
+    // Each entry is {key, page}; most recently used entry is at the front.
+    std::list<std::pair<KeyT, T>> cache_;
+
+    using ListIt = typename std::list<std::pair<KeyT, T>>::iterator;
+    std::unordered_map<KeyT, ListIt> hash_;
+    bool full() const { return (cache_.size() == size_); }
+
+public:
+    explicit LRUCache(std::size_t size) : size_(size) {}
+    std::size_t max_capacity() const { return size_; }
+    template <typename F> bool lookup_update(KeyT key, F slow_get_page)
+    {
+        if (max_capacity() == 0)
+            return false;
+
+        auto hit = hash_.find(key);
+        if (hit != hash_.end()) {
+            auto eltit = hit->second;
+            cache_.splice(cache_.begin(), cache_, eltit);
+            return true;
+        }
+
+        T page = slow_get_page(key);
+
+        if (full()) {
+            hash_.erase(cache_.back().first);
+            cache_.pop_back();
+        }
+        cache_.emplace_front(key, page);
+        hash_.emplace(key, cache_.begin());
+        return false;
+    }
+};
+
+} // namespace caches
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "lru.hpp"
+
+using Page = int;
+using PageId = int;
+
+int main()
+{
+    int cache_size, data_len;
+
+    // TODO: add input check
+    std::cin >> cache_size >> data_len;
+
+    caches::LRUCache<Page, PageId> cache{static_cast<std::size_t>(cache_size)};
+    auto load = [](PageId key) { return key; };
+    int hits = 0;
+    for (int i = 0; i < data_len; i++)
+    {
+        PageId key;
+        std::cin >> key;
+        bool hit = cache.lookup_update(key, load);
+        if (hit)
+            ++hits;
+    }
+    std::cout << hits << std::endl;
+}
