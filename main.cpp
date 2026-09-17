5f8e4381b72c4f096d8e50b2153c75a89ae208e6
LomininD <saimon1402@mail.ru>
2026-09-17 23:07:49 +0300

    Add first version of LFU cache

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include "LFU_cache.hpp"
+
+
+int slow_get_page(int key);
+
 
 int main()
 {
-    std::cout << "hello, world!\n";
+    LFUCache<int, int> cache(2);
+
+    int test_data[] = {1, 2, 1, 2, 3, 1, 2};
+
+    for(int i : test_data)
+    {
+        VERBOSE(std::cout << "Current data request: " << i << std::endl;)
+        cache.lookup_update(i, slow_get_page);
+        VERBOSE(cache.cache_dump();)
+        std::cout << "-----------" << std::endl;
+    }
+
+    return 0;
+}
+
+
+int slow_get_page(int key)
+{
+    return key;
 }
