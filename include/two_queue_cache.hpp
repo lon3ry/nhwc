acefa85c1c642230fd5e2a0486044a0e383bace8
Sergey Kovalenko <serhiosmol@gmail.com>
2026-09-22 10:18:09 +0300

    Move 2Q member functions to class definition

--- include/two_queue_cache.hpp
+++ include/two_queue_cache.hpp
@@ -12,76 +12,69 @@ namespace caches
 
 template <typename T, typename KeyT = int> class TwoQueueCache : public BaseCache<T, KeyT>
 {
-public:
-    explicit TwoQueueCache(std::size_t capacity) :
-        capacity_(capacity),
-        kin_((capacity + 3) / 4),
-        kout_((capacity + 1) / 2)
-    {};
-
-    bool lookup_update(KeyT key, std::function<T(KeyT)> slow_get_page) override;
-
-private:
     std::size_t capacity_, kin_, kout_;
 
     LRUQueue<T, KeyT> am_, a1_in_;
     GhostLRUQueue<KeyT> a1_out_;
 
-    void reclaim_for();
-
     bool page_slots_available() const { return a1_in_.size() + am_.size() < capacity_; }
     bool a1_in_above_threshold() const { return a1_in_.size() > kin_; }
     bool a1_out_above_threshold() const { return a1_out_.size() > kout_; }
 
-};
-
-template <typename T, typename KeyT>
-bool TwoQueueCache<T, KeyT>::lookup_update(KeyT key, std::function<T(KeyT)> slow_get_page)
-{
-    auto hit_am = am_.lookup(key);
-    if (hit_am)
-        return true;
-
-    auto hit_a1_out = a1_out_.has(key);
-    if (hit_a1_out)
+    void reclaim_for()
     {
-        a1_out_.erase(key);
+        if (page_slots_available())
+        {
+            return;
+        }
+        else if (a1_in_above_threshold())
+        {
+            auto item = a1_in_.pop_last_recently_used();
+            a1_out_.insert(item->first);
+            if (a1_out_above_threshold())
+                a1_out_.pop_last_recently_used();
+        }
+        else
+        {
+            am_.pop_last_recently_used();
+        }
+    }
+
+public:
+    explicit TwoQueueCache(std::size_t capacity) :
+        capacity_(capacity),
+        kin_((capacity + 3) / 4), // avoid kin_ = 0
+        kout_((capacity + 1) / 2) // same for kout_
+    {};
+
+    bool lookup_update(KeyT key, std::function<T(KeyT)> slow_get_page) override
+    {
+        auto hit_am = am_.lookup(key);
+        if (hit_am)
+            return true;
+
+        auto hit_a1_out = a1_out_.has(key);
+        if (hit_a1_out)
+        {
+            a1_out_.erase(key);
+            reclaim_for();
+            T page = slow_get_page(key);
+            am_.insert(key, page);
+            return false;
+        }
+
+        auto hit_a1_in = a1_in_.has(key);
+        if (hit_a1_in)
+            return true;
+
         reclaim_for();
+
         T page = slow_get_page(key);
-        am_.insert(key, page);
+        a1_in_.insert(key, page);
+
         return false;
     }
 
-    auto hit_a1_in = a1_in_.has(key);
-    if (hit_a1_in)
-        return true;
-
-    reclaim_for();
-
-    T page = slow_get_page(key);
-    a1_in_.insert(key, page);
-
-    return false;
-}
-
-template <typename T, typename KeyT>
-void TwoQueueCache<T, KeyT>::reclaim_for()
-{
-    if (page_slots_available())
-    {
-        return;
-    }
-    else if (a1_in_above_threshold())
-    {
-        auto item = a1_in_.pop_last_recently_used();
-        a1_out_.insert(item->first);
-        if (a1_out_above_threshold())
-            a1_out_.pop_last_recently_used();
-    }
-    else
-    {
-        am_.pop_last_recently_used();
-    }
-}
+};
 
 } //namespace caches
