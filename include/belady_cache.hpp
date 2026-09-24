e632ecab11a7035a41895bc58c6e8a02189a68b2
LomininD <saimon1402@mail.ru>
2026-09-25 00:40:04 +0300

    Optimize belady cache

--- include/belady_cache.hpp
+++ include/belady_cache.hpp
@@ -24,32 +24,42 @@ class BeladyCache
     using NodeIt = typename std::list<Record>::iterator;
     using VecIt = typename std::vector<T>::const_iterator;
 
-    NodeIt find_free_space(VecIt seq_begin, VecIt seq_end)
+    std::unordered_map<KeyT, NodeIt> cache_map_;
+    std::list<Record> cache_list_;
+
+    std::unordered_map<KeyT, std::list<std::size_t>> page_request_ind_map;
+
+    void init_page_request_ind_map(const std::vector<T> &page_request_sequence)
+    {
+        std::size_t request_vec_sz = page_request_sequence.size();
+        for (std::size_t i = 0; i < request_vec_sz; ++i)
+        {
+            KeyT cur_page_key = page_request_sequence[i];
+            page_request_ind_map[cur_page_key].push_back(i);
+        }
+    }
+
+    NodeIt find_free_space()
     {
         NodeIt victim_it = cache_list_.begin();
-        VecIt victim_request_it = seq_begin;
+        std::size_t farthest_ind = 0;
 
-        for (auto cur_page_it = cache_list_.begin(); cur_page_it != cache_list_.end();
-                                                                                    ++cur_page_it)
+        for (auto it = cache_list_.begin(); it != cache_list_.end(); ++it)
         {
-            auto next_it = find(seq_begin, seq_end, cur_page_it->key);
-            if (next_it == seq_end)
-                return cur_page_it;
-            else
+            auto &cur_ind_list = page_request_ind_map[it->key];
+            if (cur_ind_list.empty())
+                return it;
+
+            std::size_t next_ind = cur_ind_list.front();
+            if (next_ind > farthest_ind)
             {
-                if (next_it > victim_request_it)
-                {
-                    victim_request_it = next_it;
-                    victim_it = cur_page_it;
-                }
+                farthest_ind = next_ind;
+                victim_it = it;
             }
         }
 
         return victim_it;
     }
-
-    std::unordered_map<KeyT, NodeIt> cache_map_;
-    std::list<Record> cache_list_;
 
 public:
     BeladyCache(std::size_t capacity) : capacity_(capacity) {}
@@ -58,16 +68,21 @@ public:
     bool is_full() const { return (capacity_ == cache_map_.size()); }
 
     // do we need size_t here?
-    std::size_t calculate_hits(const std::vector<T> page_request_sequence)
+    std::size_t calculate_hits(const std::vector<T> &page_request_sequence)
     {
         if (max_capacity() == 0)
             return 0; // no hits possible
 
+        init_page_request_ind_map(page_request_sequence);
+
         std::size_t hits = 0;
 
-        for (auto it = page_request_sequence.begin(); it != page_request_sequence.end(); ++it)
+        for (std::size_t i = 0; i < page_request_sequence.size(); ++i)
         {
-            KeyT key = *it;
+            KeyT key = page_request_sequence[i];
+
+            page_request_ind_map[key].pop_front();
+
             if (auto page_it = cache_map_.find(key); page_it != cache_map_.end())
             {
                 hits++;
@@ -76,12 +91,12 @@ public:
             {
                 if (is_full())
                 {
-                    auto node_it = find_free_space(it, page_request_sequence.end());
+                    auto node_it = find_free_space();
                     cache_map_.erase(node_it->key);
                     cache_list_.erase(node_it);
                 }
 
-                Record new_page = {.key = key, .page = key}; // add slow get page
+                Record new_page = {.key = key, .page = key}; // add slow get page?
                 cache_list_.push_front(new_page);
                 cache_map_[key] = cache_list_.begin();
             }
